08c4511ec0842381bcb1da09bfec8343ca994afc
Phillip K <34702189+gaterunner341@users.noreply.github.com>
2023-07-31 09:15:44 -0400

    Update QRDecode.sh

--- QRDecode.sh
+++ QRDecode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Decode QR Code
 # Python code credit Hoang Vo
-decoded_data=$(python3 QRTools.py $1)
+decoded_data=$(python3 QRDecode.py $1)
 # Pass decoded data to defang
 echo "Output data for $1:"
 echo "$decoded_data" | defang
